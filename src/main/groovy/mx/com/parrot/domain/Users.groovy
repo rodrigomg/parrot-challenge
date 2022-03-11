8ff38d9defcb65576f39ad6515de9932c8b77530
technosoul <rockdrigo.mtz@gmail.com>
2022-03-10 19:54:49 -0600

    create users domain

---
+++ src/main/groovy/mx/com/parrot/domain/Users.groovy
@@ -0,0 +1,38 @@
+package mx.com.parrot.domain
+
+import javax.persistence.*
+import java.io.Serializable
+import java.util.Set
+import groovy.transform.ToString
+
+@Entity
+@Table(name = "users", schema = "parrot")
+class Users implements Serializable {
+
+  static final long serialVersionUID = 1L
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id
+
+  @Column(name = "email")
+  String email
+
+  @Column(name = "first_name")
+  String firstName
+
+  @Column(name = "last_name")
+  String lastName
+
+  @Override
+  String toString() {
+    """
+    Users{
+      id=${id}
+      email=${email}
+      firstName=${firstName}
+      lastName=${lastName}
+    }
+    """
+  }
+}
