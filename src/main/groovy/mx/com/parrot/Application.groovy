6b718cda837c456a1ecc5fb36851dd6125009d74
technosoul <rockdrigo.mtz@gmail.com>
2022-04-01 10:09:59 -0600

    add swagger support :smile:

--- src/main/groovy/mx/com/parrot/Application.groovy
+++ src/main/groovy/mx/com/parrot/Application.groovy
@@ -2,7 +2,21 @@ package mx.com.parrot
 
 import io.micronaut.runtime.Micronaut
 import groovy.transform.CompileStatic
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 
+@OpenAPIDefinition(
+  info = @Info(
+    title = "parrot service",
+    version = "0.1",
+    description = "Parrot APIs"
+  )
+)
+@SecurityScheme(
+  name = "basicAuth", type = SecuritySchemeType.HTTP, scheme = "basic", description = "Input your username and password to access this API"
+)
 @CompileStatic
 class Application {
   static void main(String[] args) {
